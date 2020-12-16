886c0584957ff458ca8e0181ae421aa932b57f5f
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2020-12-16 18:26:09 +0100

    Commit with corrected tests

--- src/test/java/CreditCalculator/CalculatorTest.java
+++ src/test/java/CreditCalculator/CalculatorTest.java
@@ -1,37 +1,38 @@
 package CreditCalculator;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 
 public class CalculatorTest {
 
-    final int creditAmount  = 150000;
+    final int creditAmount = 150000;
     final int dueDateInMonths = 24;
     final int annualPercent = 48;
     Calculator calculator = new Calculator();
 
     @Test
-    public void testMonthlyPaymentCalculation (){
-        final double result = 9837.82;
-        final double monthlyPayment = calculator.monthlyPaymentCalculation(creditAmount,dueDateInMonths,annualPercent);
-        Assertions.assertEquals(result,monthlyPayment);
+    public void testMonthlyPaymentCalculation() {
+        final double result = 9838.02;
+        final double monthlyPayment = calculator.monthlyPaymentCalculation(creditAmount, dueDateInMonths, annualPercent);
+        Assertions.assertEquals(result, monthlyPayment);
 
     }
 
     @Test
-    public void testCalculationOfTheTotalAmountToBeReturnedToTheBank(){
-        final double result = 236107.68;
+    public void testCalculationOfTheTotalAmountToBeReturnedToTheBank() {
+        final double result = 236112.48;
         final double totalAmount = calculator.calculationOfTheTotalAmountToBeReturnedToTheBank(creditAmount,
-                dueDateInMonths,annualPercent);
-        Assertions.assertEquals(result,totalAmount);
+                dueDateInMonths, annualPercent);
+        Assertions.assertEquals(result, totalAmount);
     }
 
     @Test
-    public void testOverpaymentCalculationForTheEntirePeriod(){
-        final double result = 86107.68;
-        final double overpayment = calculator.overpaymentCalculationForTheEntirePeriod(creditAmount,dueDateInMonths,
+    public void testOverpaymentCalculationForTheEntirePeriod() {
+        final double result = 86112.48;
+        final double overpayment = calculator.overpaymentCalculationForTheEntirePeriod(creditAmount, dueDateInMonths,
                 annualPercent);
-        Assertions.assertEquals(result,overpayment);
+        Assertions.assertEquals(result, overpayment);
     }
 
 }
